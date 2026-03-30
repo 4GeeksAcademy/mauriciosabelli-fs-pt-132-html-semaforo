8bd502e93167ebcecfc2589d5e9d74ef28409808
Mauricio Sabelli Machado <mnsabelli@gmail.com>
2026-03-30 18:42:33 +0000

    Semaforo opertivo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,18 +1,18 @@
 import {useState} from "react";
 
 const Semaforo = () => {
-    const [ color, setColor] = useState("red");
+    const [color, setColor] = useState("");
  
      return (
          <div>
-             <div className="palito"></div>
-             <div className = "luces">
-                 <div onClick={() => setColor("red")} className={"light-red" + (color === "red" ? " brillo" : "" )}></div>
-                 <div onClick={() => setColor("yellow")} className={"light-yellow" + (color === "yellow" ? " brillo" : "" )}></div>
-                 <div onClick={() => setColor("green")} className={"light-green" + (color === "green" ? " brillo" : "" )}></div>
+             <div className="palito"></div>                
+             <div className="luces">
+                <div onClick={() => setColor("red")} className={"light-red" + (color === "red" ? " brillo" : "" )}></div>
+                <div onClick={() => setColor("yellow")} className={"light-yellow" + (color === "yellow" ? " brillo" : "" )}></div>
+                <div onClick={() => setColor("green")} className={"light-green" + (color === "green" ? " brillo" : "" )}></div>
              </div>
          </div>
      );
- };
-
- export default Semaforo;
+    };
+    
+export default Semaforo;
